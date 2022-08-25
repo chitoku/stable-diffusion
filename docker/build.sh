a63835f5a7a016c43e8c25c8157cd97a9b658931
Chitoku YATO <cyato@nvidia.com>
2022-08-25 03:05:33 -0700

    Push container to GitHub Container registry

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-sudo docker run -it --rm --net=host --runtime nvidia \
-    -w /stable-diffusion \
-    -v ${PWD}:/stable-diffusion
-    stable-diffusion .
-
 sudo docker build -t stable-diffusion .
--- docker/run.sh
+++ docker/run.sh
@@ -3,5 +3,6 @@
 sudo docker run -it --rm --net=host --runtime nvidia \
     -w /stable-diffusion \
     -v ${PWD}:/stable-diffusion \
-    stable-diffusion 
-    python3 scripts/txt2img.py --prompt "5 students raising their hands in a class room" --plms
+    nvcr.io/chitoku/stable-diffusion:jetson 
+
+#    python3 scripts/txt2img.py --prompt "5 students raising their hands in a class room" --plms
