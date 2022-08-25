f7cd16326d7aff5f51fc64203aefde0cb717f1f5
Chitoku YATO <cyato@nvidia.com>
2022-08-25 03:04:08 -0700

    Undo trying to remove large files

--- scripts/bigfile_split.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f ./assets/results.gif ]; then
-    split -b 4M ./assets/results.gif ./assets/results.gif.
-fi
-
-if [ -f ./data/imagenet_train_hr_indices.p ]; then
-    split -b 4M ./data/imagenet_train_hr_indices.p ./data/imagenet_train_hr_indices.p.
-fi
